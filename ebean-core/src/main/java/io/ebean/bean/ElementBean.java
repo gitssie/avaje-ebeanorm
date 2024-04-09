d68e422f4b549d074f6f2960b27265f092904247
jessie <jessie_6@126.com>
2024-04-09 11:02:21 +0800

    1、增加动态实体一些通过用方法

--- ebean-core/src/main/java/io/ebean/bean/ElementBean.java
+++ ebean-core/src/main/java/io/ebean/bean/ElementBean.java
@@ -1,9 +1,11 @@
 package io.ebean.bean;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import io.ebeaninternal.server.core.BasicTypeConverter;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.util.*;
 
 public final class ElementBean implements EntityBean, Map<String, Object> {
 
@@ -195,5 +197,128 @@ public final class ElementBean implements EntityBean, Map<String, Object> {
     ToStringBuilder builder = new ToStringBuilder();
     toString(builder);
     return builder.toString();
+  }
+
+
+  public UUID getUUID(String name) {
+    return BasicTypeConverter.toUUID(get(name), true);
+  }
+
+  public Boolean getBoolean(String name) {
+    return BasicTypeConverter.toBoolean(get(name), "true");
+  }
+
+  public boolean getBooleanValue(String name) {
+    return getBooleanValue(name, false);
+  }
+
+  public boolean getBooleanValue(String name, boolean val) {
+    Boolean booleanVal = getBoolean(name);
+    return booleanVal == null ? val : booleanVal.booleanValue();
+  }
+
+  public Integer getInteger(String name) {
+    return BasicTypeConverter.toInteger(get(name));
+  }
+
+  public int getIntegerValue(String name) {
+    return getIntegerValue(name, 0);
+  }
+
+  public int getIntegerValue(String name, int val) {
+    Integer intVal = BasicTypeConverter.toInteger(get(name));
+    return intVal == null ? val : intVal.intValue();
+  }
+
+  public BigDecimal getBigDecimal(String name) {
+    return BasicTypeConverter.toBigDecimal(get(name));
+  }
+
+  public BigDecimal getBigDecimalValue(String name) {
+    return getBigDecimalValue(name, BigDecimal.ZERO);
+  }
+
+  public BigDecimal getBigDecimalValue(String name, BigDecimal val) {
+    BigDecimal bigVal = BasicTypeConverter.toBigDecimal(get(name));
+    return bigVal == null ? val : bigVal;
+  }
+
+  public BigInteger getBigInteger(String name) {
+    return BasicTypeConverter.toMathBigInteger(get(name));
+  }
+
+  public BigInteger getBigIntegerValue(String name) {
+    return getBigIntegerValue(name, BigInteger.ZERO);
+  }
+
+  public BigInteger getBigIntegerValue(String name, BigInteger val) {
+    BigInteger bigVal = BasicTypeConverter.toMathBigInteger(get(name));
+    return bigVal == null ? val : bigVal;
+  }
+
+  public Long getLong(String name) {
+    return BasicTypeConverter.toLong(get(name));
+  }
+
+  public long getLongValue(String name) {
+    return getLongValue(name, 0);
+  }
+
+  public long getLongValue(String name, long val) {
+    Long longVal = BasicTypeConverter.toLong(get(name));
+    return longVal == null ? val : longVal.longValue();
+  }
+
+  public Double getDouble(String name) {
+    return BasicTypeConverter.toDouble(get(name));
+  }
+
+  public double getDoubleValue(String name) {
+    return getDoubleValue(name, 0);
+  }
+
+  public double getDoubleValue(String name, double val) {
+    Double dVal = BasicTypeConverter.toDouble(get(name));
+    return dVal == null ? val : dVal.doubleValue();
+  }
+
+  public Float getFloat(String name) {
+    return BasicTypeConverter.toFloat(get(name));
+  }
+
+  public float getFloatValue(String name) {
+    return getFloatValue(name, 0);
+  }
+
+  public float getFloatValue(String name, float val) {
+    Float fVal = BasicTypeConverter.toFloat(get(name));
+    return fVal == null ? val : fVal.floatValue();
+  }
+
+  public String getString(String name) {
+    return BasicTypeConverter.toString(get(name));
+  }
+
+  public String getString(String name, String val) {
+    String strVal = BasicTypeConverter.toString(get(name));
+    return strVal == null ? val : strVal;
+  }
+
+  public Date getDate(String name) {
+    return BasicTypeConverter.toUtilDate(get(name));
+  }
+
+  public Date getDate(String name, Date val) {
+    Date dateVal = BasicTypeConverter.toUtilDate(get(name));
+    return dateVal == null ? val : dateVal;
+  }
+
+  public Timestamp getTimestamp(String name) {
+    return BasicTypeConverter.toTimestamp(get(name));
+  }
+
+  public Timestamp getTimestamp(String name, Timestamp val) {
+    Timestamp tVal = BasicTypeConverter.toTimestamp(get(name));
+    return tVal == null ? val : tVal;
   }
 }
